fff8c8a25caa3c507f14d88cf52ad2e0f0435a05
illfriday <willprouty@gmail.com>
2021-08-04 23:17:13 -0700

    fix uncontrolled form in Header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -30,6 +30,7 @@ export default class Header extends Component {
     this.toggleNav = this.toggleNav.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
+    //We are using the built-in jS bind() METHOD on the 'Header' CLASS COMPONENT's METHODS so they can refer to 'Header' when using the 'this' KEYWORD. This is so we don't use the internal FUNCTION SCOPE of 'this' inside the METHODS(FUNCTIONS) and can access and setState() the PROPERTIES stored in STATE
   }
 
   toggleNav() {
@@ -45,9 +46,9 @@ export default class Header extends Component {
   }
 
   handleLogin(event) {
-    console.log("hi");
+    // console.log("hi");
     alert(
-      `Name:${this.username.value} Password: ${this.password.value} Remember: ${this.remember}`
+      `Name:${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`
     );
     this.toggleModal();
     event.preventDefault();
@@ -111,20 +112,34 @@ export default class Header extends Component {
         </Navbar>
 
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-          <ModalHeader>Login</ModalHeader>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
           <ModalBody>
             <Form onSubmit={this.handleLogin}>
               <FormGroup>
                 <Label htmlFor="username">Username</Label>
-                <Input type="text" id="username" name="username" innerRef={input => this.username = input } />
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  innerRef={(input) => (this.username = input)}
+                />
               </FormGroup>
               <FormGroup>
                 <Label htmlFor="password">Password</Label>
-                <Input type="password" id="password" name="password" innerRef={ input => this.password = input } />
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  innerRef={(input) => (this.password = input)}
+                />
               </FormGroup>
               <FormGroup check>
                 <Label check>
-                  <Input type="checkbox" id="remember" name="remember" innerRef={input => this.remember = input } />
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={(input) => (this.remember = input)}
+                  />
                   Remember Me
                 </Label>
               </FormGroup>
@@ -143,3 +158,11 @@ export default class Header extends Component {
     );
   }
 }
+
+//we are creating a Modal with a Login form below the Navbar. Setting built-in 'reactstrap' attributes of 'isOpen' and 'toggle'. 'isOpen' is tied to the BOOLEAN STATE property 'isModalOpen'. When we 'toggle' the MODAL, we run the METHOD 'toggleModal()' on the 'Header' CLASS COMPONENT. 'toggleModal()' sets the BOOLEAN STATE property 'isModalOpen' to the opposite of it's current BOOLEAN VALUE(true= false)
+
+//toggle='toggleModal" on the ModalHeader provides a way to close the MODAL
+//We attach an onCLick ATTRIBUTE  of 'toggleModal()' on a <Button> inside the <Collapse> 'reactstrap' COMPONENT so we can view the button when the MODAL is collapsed
+
+//we are creating an UNCONTROLLED FORM inside the <Modal>. This <Form> will contain it's own "source of truth" separate from the STATE. 3 'reactstrap' <FormGroup>'s with <Input> s and <Label>s(htmlFor= instead of for= IN JSX). These are similar to HTML elements.
+//set up 'handleLogin()' METHOD on the "Header" CLASS COMPONENT. Prevents page from reloading event.'preventDefault()' and closes the MODEL 'toggleModal()'. We are attaching 'innerRefs' to the <Input>s inside the <Form>,  matching the name= ATTRIBUTE of the <Input>. these innerRefs are passed inside the EVENT HANDLER 'onClick' of the SUBMIT calling 'handleLogin()'. We can access the VALUE of the input inside 'handleLogin' METHOD
